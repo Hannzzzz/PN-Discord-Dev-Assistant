45d35f96a3de416383cc8b21f1bcc38712eeca90
zenliuy <bintangshelock20@gmail.com>
2026-09-20 16:17:17 +0700

    Add welcome plugin

---
+++ bot/plugins/welcome/welcome.py
@@ -0,0 +1,65 @@
+import discord
+from discord.ext import commands
+
+
+class Welcome(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+
+        channel = discord.utils.get(
+            member.guild.text_channels,
+            name="general"
+)
+
+        if channel :
+            file = discord.File("welcome.png", filename="welcome.png")
+
+            member_count= member.guild.member_count
+
+            embed = discord.Embed(
+                title = "🎉 SELAMAT DATANG!",
+                description=(
+                f"Welcome {member.mention}! 👋\n "
+                f"Selamat datang di\n"
+                f"**{member.guild.name}**\n\n"
+                f" Kamu adalah member ke-{member_count}\n"
+                f"Semoga betah disini dan enjoy yak!\n"
+            )
+        )
+
+            embed.add_field(
+             name="📌 START HERE",
+
+            value=(
+            "📖 Baca rules di <#CHANNEL_ID>\n"
+            "💬 Ngobrol di <#CHANNEL_ID>\n"
+            "🎮 Cari teman mabar\n"
+            "🤝 Kenalan sama member lain"
+            
+             ),
+            inline=False
+        )
+
+            embed.set_thumbnail(
+                url=member.display_avatar.url
+        )
+
+            embed.set_image(
+                url="attachment://welcome.png"
+        )
+
+            embed.set_footer(
+                text="PN DISCORD • DEV ASSISTANT"
+        )
+
+            await channel.send(
+                embed=embed,
+                file=file
+        )
+    
